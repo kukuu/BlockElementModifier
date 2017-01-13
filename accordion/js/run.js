39df6ed890fb2cac63fe0453077201b461d25528
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2017-01-13 13:24:01 +0000

    Adding BEM modules and dependencies

---
+++ accordion/js/run.js
@@ -0,0 +1,9 @@
+//Author: Alexander Adu-Sarkodie
+var $title = $('.js-title');
+var copy   = '.js-copy';
+
+$title.click(function () {
+  $(this).next(copy).slideToggle();
+  $(this).parent().siblings().children().next().slideUp();
+  return false;
+});
